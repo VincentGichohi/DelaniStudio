0e33cef88a8fc1653f188bd4b3d9029408776eff
VincentGichohi <vincentegichohi@gmail.com>
2022-03-15 08:45:19 +0300

    Added a javascript folder with a popup button

---
+++ js/script.js
@@ -0,0 +1,13 @@
+//a function for popping up the message once the submit button is clicked
+let popup = document.getElementById("popup");
+
+function openPopUp() {
+    popup.classList.add("open-popup");
+    }
+function closePopUp() {
+    popup.classList.remove("open-popup");
+}
+//a function for displaying the message once the image has been clicked
+// $(document).ready(function() {
+//     $()
+// })
